0b344e6b3daad5e87eda7fe08e59eae3db7f7462
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2016-09-29 14:39:07 -0400

    Create MrCoxallStack.java

---
+++ MrCoxallStack.java
@@ -0,0 +1,22 @@
+/****************************************************************************
+ *
+ * Created by: Jess and Craig
+ * Created on: Sept 2016
+ *
+ ****************************************************************************/
+import java.util.ArrayList;
+
+public class MrCoxallStack {
+
+	ArrayList<Integer> al = new ArrayList<Integer>();
+	
+	
+	public void push (int numbah) {
+		
+		 al.add(numbah);
+		
+	}
+
+	
+	
+}
